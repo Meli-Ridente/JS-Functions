1541dfafd3e9a1fc70a94e848f58abb461b342bb
Melissa Ridente <melissaridente3e@gmail.com>
2022-12-05 19:49:00 +0100

    Iteración #1: Buscar el máximo

---
+++ ejercicio1.js
@@ -0,0 +1,9 @@
+/*Completa la función que tomando dos números como argumento devuelva el más alto. */
+
+function numeros(num1, num2){
+  if (num1 > num2) return num1
+    else return num2
+}
+
+
+console.log(numeros(19, 22));
